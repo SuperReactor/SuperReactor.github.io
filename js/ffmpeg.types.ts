f84645b75d71b867c70b5a75aeed222c7ca09b3e
SuperReactor <118234873+SuperReactor@users.noreply.github.com>
2026-03-26 19:11:57 -0700

    windows XP
    dont open ts or ur cooked

--- js/ffmpeg.types.ts
+++
@@ -1,7 +0,0 @@
-export class FfmpegProgress implements MonochromeProgress {
-    constructor(
-        public readonly stage: 'loading' | 'encoding' | 'finalizing',
-        public readonly progress: number,
-        public readonly message?: string
-    ) {}
-}
